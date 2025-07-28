4b65d39f8d95384b3a948fa3d79eafebe32e093e
tai21042004 <tai21042004@gmail.com>
2025-07-28 22:14:51 +0700

    feat(api): add Makefile and database setup (G)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	_ "github.com/lib/pq"
 	"gitlab.com/tantai-kanban/kanban-api/config"
 	"gitlab.com/tantai-kanban/kanban-api/internal/appconfig/minio"
 	"gitlab.com/tantai-kanban/kanban-api/internal/appconfig/postgre"
